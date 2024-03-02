a54d170b0bc95d3bfb78e2881c60d208c0acd195
Freddy <perezgfreddyj@gmail.com>
2024-03-01 23:29:56 -0500

    Tablas en Boostrap de alumnos y cursos para practicar Typesccript

---
+++ src/app/components/tabla-alumnos/mocks.ts
@@ -0,0 +1,22 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] =[
+    {
+    nombre:'Ricardo',
+    apellido: 'Perez',
+    promedio: 9
+},
+{
+    nombre:'Florencia',
+    apellido: 'Gonzalez',
+    promedio: 8.0
+
+},
+{
+    nombre:'Pedro',
+    apellido: 'Francel',
+    promedio: 7.0
+
+},
+
+]
---
+++ src/app/components/tabla-cursos/mocks.ts
@@ -0,0 +1,28 @@
+import { Curso } from "src/app/models/curso-to";
+
+export const cursos: Curso[] =[
+    {
+    materia:'JavasCript',
+    dificultad: 9,
+    requiere: 'HTML y CCs'
+},
+{
+    materia:'TypeScript',
+    dificultad: 7,
+    requiere: 'JavasCript'
+
+},
+{
+    materia:'Angular',
+    dificultad: 9,
+    requiere: `HTML, CCS, JS y TS`
+
+},
+{
+    materia:'React',
+    dificultad: 7,
+    requiere: `HTML, CCS y JS`
+
+},
+
+]
